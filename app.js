4945abe6719ca1bf126d2089438ef2445f5841c5
nsym7372 <nsym7372@outlook.jp>
2020-05-13 23:36:19 +0900

    express利用

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var http = require("http");
-var server = http.createServer(function(req, res){
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Hello World");
+var express = require("express");
+
+var app = express();
+
+app.get("/", function(req, res){
+    return res.send("Hello express");
 });
+
+var server = http.createServer(app);
 
 server.listen(3000);
