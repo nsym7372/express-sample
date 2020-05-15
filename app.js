be03830e1732a60ab611edaffc087aa5c12db71d
nsym7372 <nsym7372@outlook.jp>
2020-05-16 06:24:18 +0900

    post→値取得できた

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 var http = require("http");
 var express = require("express");
+var morgan = require("morgan");
+var path = require("path");
+var bodyParser = require("body-parser");
 
 var app = express();
 
+app.set("views", path.join(__dirname, "templates"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: false}));
+
+
 app.get("/", function(req, res){
-    return res.send("Hello express");
+    return res.render("index", {title: "Hello World"});
+    // return res.send("Hello world");
 });
 
+app.get("/form", function(req, res){
+    return res.render("form");
+})
+
+app.post("/form", function(req, res){
+    return res.render("result", {username: req.body.username, message: req.body.message})
+});
+
+app.use(morgan("combined"));
 var server = http.createServer(app);
 
 server.listen(3000);
