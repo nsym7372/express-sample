ee41432f150dc8dfc9f2baacd2650e90c5fb0f86
nsym7372 <nsym7372@outlook.jp>
2020-05-17 23:44:52 +0900

    スタイルシート適用

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var app = express();
 app.set("views", path.join(__dirname, "templates"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: false}));
+app.use("/static", express.static(path.join(__dirname, "styles")));
 
 
 app.get("/", function(req, res){
